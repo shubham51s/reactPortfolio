9cc743abb42846f1d869f229e6cd2108419d6617
shubham51s <shubhmasolat51@gmail.com>
2024-09-21 20:21:03 +0530

    resume file changed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./homecomponent.module.css";
-import resume from "../../assets/shubhams.pdf";
+import resume from "../../assets/shubham.pdf";
 
 function HomeComp({ setActiveNav, homeRef }) {
   return (
